c53c3f4ddc0be86d1a8de67cfff24283b87fd55c
Fabricio Mafuresco <f.mafu@hotmail.com>
2024-11-15 17:37:59 -0300

    Ejercicio Hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejercicio8';
+  usuario = {
+    nombre: '',
+    email: '',
+    password: ''
+  };
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Datos del formulario:', this.usuario);
+    } else {
+      alert('Ingrese los datos correctos en los campos');
+    }
+  }
 }
